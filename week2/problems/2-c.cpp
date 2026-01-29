bf19cd977668702cca67fc25b533d8656d9c14b6
Ahn jae min <ajmin0421@naver.com>
2026-01-29 11:24:15 +0000

    feat: Solve 'Safety Area' using DFS with various rain thresholds

--- week2/problems/2-c.cpp
+++ week2/problems/2-c.cpp
@@ -6,13 +6,13 @@ int dx[] = {0,1,0,-1};
 int n,temp,ret=1,rain,highest,a[max_n][max_n],visited[max_n][max_n];
 
 void dfs(int y,int x){
+    visited[y][x] =1;
     for(int i=0;i<4;i++){
         int ny=y+dy[i];
         int nx=x+dx[i];
         if(ny<0||ny>=n||nx<0||nx>=n) continue;
         if(a[ny][nx]<=rain) continue;
         if(visited[ny][nx]) continue;
-        visited[ny][nx] =1;
         dfs(ny,nx);
     }
 }
